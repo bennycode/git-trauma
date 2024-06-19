3612b252baceea2e641833f3b91e0f3ebd1029b2
Benny Neugebauer <mail@bennycode.com>
2024-06-19 19:16:15 +0300

    make branch name detection resilient

--- src/inspect/getBranchName.ts
+++ src/inspect/getBranchName.ts
@@ -1,13 +1,23 @@
 import {input} from '@inquirer/prompts';
 import type {SimpleGit} from 'simple-git';
 
+async function getCurrentBranchName(git: SimpleGit) {
+  try {
+    const branchSummary = await git.branch();
+    const currentBranch = branchSummary.branches[branchSummary.current]?.name;
+    return currentBranch ?? 'main';
+  } catch {
+    // We catch errors because when Git files are corrupt the branch name won't be retrieved successfully
+    return 'main';
+  }
+}
+
 export async function getBranchName(git: SimpleGit): Promise<string> {
   console.log(`Detecting current branch name...`);
-  const branchSummary = await git.branch();
-  const currentBranch = branchSummary.branches[branchSummary.current]?.name;
+  const currentBranch = await getCurrentBranchName(git);
 
   const answer = await input({
-    default: currentBranch ? currentBranch : 'main',
+    default: currentBranch,
     message: 'Which branch would you like to use?',
   });
 

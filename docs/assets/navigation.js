3d2f51e55e6062b7befeb2f9e849073653a013d9
Benny Neugebauer <mail@bennycode.com>
2024-06-19 18:21:49 +0300

    chore: Generated release files

--- docs/assets/navigation.js
+++ docs/assets/navigation.js
@@ -1,2 +1,2 @@
 window.navigationData =
-  'data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslLyrXTOSSwuVtJRKkgsyQAKJIO4qcX6UAm9jJLcHKBsdmZeipKVoZFFbSwAMUbVPD0AAAA=';
+  'data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslJKzywJKUoszU1U0lEqSCzJULJSSivNSy7JzM8r1odL6mWU5OYo6ShlZ+alKFmZmdTGAgBBC5YPQgAAAA==';
